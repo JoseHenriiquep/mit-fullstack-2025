4fe30bd1eb884ab10bd4965c4344a44e82768d6c
JoseHenriiquep <josehenriquenetinho@gmail.com>
2025-02-13 22:03:35 -0300

    Dependencias Instaladas + Instalando Template

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.png'
+
+const Header = () => {
+    return(
+        <>
+            <img src={logo} alt="logo"/>
+            <h1>My Header</h1>
+        </>
+    )
+};
+
+export default Header;
