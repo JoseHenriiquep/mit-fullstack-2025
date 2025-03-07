c88f767065ddcec8612b145e11f5afeb4be6acc4
JoseHenriiquep <josehenriquenetinho@gmail.com>
2025-03-06 21:40:51 -0300

    Feedback refactor

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Card } from "reactstrap";
+import { Container, Card, Form, FormGroup, Input, Label, Button, Alert, Spinner } from "reactstrap";
 
 const Feedback = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
     const [formData, setFormData] = useState(
         {
             nome: '',
@@ -12,6 +9,10 @@ const Feedback = () => {
             feedback: '',
         }
     );
+    const [submited, setSubmited] = useState(false)
+    const [userNames, setUserNames] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
     
@@ -22,7 +23,8 @@ const Feedback = () => {
               throw new Error("Erro ao buscar os dados")
             }
             const data = await response.json();
-            setUsers(data)
+            const names = Array.from(new Set(data.map(user => user.nome)))
+            setUserNames(names);
           } catch (error) {
             setError(error.message);
             console.error("Sem resposta", error)
@@ -39,47 +41,79 @@ const Feedback = () => {
         
 
         if (formData.nome === "") {
-            alert("Preencha seu nome")
-        } else if (formData.email === "") {
-            alert("Preencha seu email")
+            setError("Preencha seu nome");
+            return false;
+        } else if (formData.email.includes("infnet")) {
+            setError("O email precisa ser Institucional");
+            return false;
         } else if (formData.feedback === "") {
-            alert("Preencha seu feedback")
+            setError("Preencha seu feedback");
+            return false;
         } else {
-            const confirmationMessage = `
-            Olá ${formData.nome}, <${formData.email}> seu feedback foi enviado!
-            `
-            alert(confirmationMessage)
+            setError('')
+            setSubmited(true)
         }   
     }
 
+    if (loading) return (
+        <Container>
+            <Spinner color="primary"/>
+            <p>Carregando usuarios...</p>
+        </Container>
+    )
+
     return(
-        <div>
+        <Container>
             <h2>Feedback</h2>
-            <Card>
-                <form onSubmit={handleSubmit}>
-                    <input 
-                    name="nome"
-                    type="text" 
-                    value={formData.nome}
-                    placeholder="Nome"
-                    onChange={(e) => setFormData({...formData, nome: e.target.value})}
-                    />
-                    <input 
-                    name="email"
-                    type="email" 
-                    value={formData.email} 
-                    placeholder="E-mail" 
-                    onChange={(e) => setFormData({...formData, email: e.target.value})}
-                    />
-                    <textarea
-                    value={formData.feedback}
-                    placeholder="feedback" 
-                    onChange={(e) => setFormData({...formData, feedback: e.target.value})}
-                    />
-                    <button type='submit'>Enviar</button>
-                </form>
-            </Card>
-        </div>
+            <Form onSubmit={handleSubmit}>
+                <Card body>
+                    {error && <Alert color="danger">{error}</Alert>}
+                    {!error && submited && 
+                    <Alert color="success">Olá {formData.nome}, {formData.email}  seu feedback foi enviado!
+                    </Alert>}
+                    <FormGroup>
+                        <Label for="nome">Nome:</Label>
+                        <Input 
+                        name="nome"
+                        id="nome"
+                        type="select" 
+                        value={formData.nome}
+                        onChange={(e) => setFormData({...formData, nome: e.target.value})}
+                        required
+                        >
+                        <option value="">Selecionar usuário</option>
+                        {userNames.map((name, index) => (
+                            <option key={index} value={name}>{name}</option>
+                        ))}
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input 
+                        name="email"
+                        type="email" 
+                        value={formData.email} 
+                        placeholder="E-mail" 
+                        onChange={(e) => setFormData({...formData, email: e.target.value})}
+                        />
+                    </FormGroup>
+                   <FormGroup>
+                        <Label for="nome">Feedback</Label>
+                        <Input
+                        type="textarea"
+                        name="feedback"
+                        value={formData.feedback}
+                        placeholder="Feedback" 
+                        onChange={(e) => setFormData({...formData, feedback: e.target.value})}
+                        />
+                        <small className="text-muted">
+                            {formData.feedback.length} caracters(mínimo 100)
+                        </small>
+                    </FormGroup>
+                    <Button color="primary" type='submit'>Enviar</Button>
+                </Card>
+            </Form>
+        </Container>
     );
 }
 
