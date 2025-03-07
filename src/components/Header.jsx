c080143bb403ca3dfffe2e8fb32cda3fb907e4a9
JoseHenriiquep <josehenriquenetinho@gmail.com>
2025-03-06 21:44:11 -0300

    projeto v0.9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
 import logo from '../assets/logo.png'
+import React, { useState } from 'react';
+import { Nav, Navbar, NavbarToggler, Collapse, NavbarBrand, NavItem, NavLink, Container } from "reactstrap"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
         <>
-            <img src={logo} alt="logo"/>
-            <h1>My Header</h1>
+            <Navbar color='dark' dark expand="md">
+                <NavbarBrand>
+                <img src={logo} width="50px" alt="logo" style={{paddingRight: "5px"}}/>
+                MIT Full Stack 2025
+                </NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className='ms-auto' navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/users">
+                                Users
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/feedback">
+                                Feedback
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>  
+            </Navbar>
         </>
     )
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,37 @@
+import styled from "styled-components";
 import Timer from "./Timer";
 import React, { useState } from "react";
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    min-height: 100vh;
+    background: #f8f9fa;
+    border-left: 2px solid #000;
+    border-right: 2px solid #000;
+    `;
+
+    const Title = styled.h1`
+        font-size: 3rem;
+        color: #343a40;
+        margin-bottom: 1rem;
+    `;
+
 const Home = () => {
+    const currDate = new Date();
+
     const [isRunning, setIsRunning] = useState(true);
-
-    const handleStart = () => {
-        setIsRunning(true);
-    };
-
-    const handleStop = () => {
-        setIsRunning(false);
-    };
-
-    const toggleTimer = () => {setIsRunning(!setIsRunning)}
+    const toggleTimer = () => {setIsRunning(!isRunning)}
 
     return(
-        <>              
-            <h1>Home</h1>
+        <Container>              
+            <Title>Home</Title>
             <Timer isRunning={isRunning}/>
             <button onClick={toggleTimer}>{isRunning?'Stop':'Start'}</button>
-        </>
+        </Container>
     );
 }
 
